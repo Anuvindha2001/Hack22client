2b781c88ddf51de84ac1969945fdb02faa07cff5
Anuvindha2001 <b19cs015@mace.ac.in>
2021-11-28 00:28:13 +0530

    faq,fa fcons, title align

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+
+import $ from "jquery";
+
 $(document).ready(function () {
     $('.customer-logos').slick({
         slidesToShow: 4,
@@ -23,3 +26,17 @@ $(document).ready(function () {
         ]
     })
 })
+
+let items = document.querySelectorAll('.faq');
+
+let length = items.length; 
+
+items.forEach(
+  (item) => {
+    item.addEventListener('click', function(){
+      item.children[1].style.display = item.children[1].style.display==='block' ? item.children[1].style.display = 'none' : item.children[1].style.display = 'block';
+      item.children[0].classList.toggle('to-bottom');
+      item.children[0].classList.toggle('to-top');
+    })
+  }
+)
