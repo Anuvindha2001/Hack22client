d37f9c4dd8d66315aedca34cf1119c77d2ab7599
Anuvindha2001 <b19cs015@mace.ac.in>
2021-11-29 23:45:17 +0530

    small changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 
-import $ from "jquery";
-
+$(window).on('load', function () {
+    $('#preloader').hide();
+  }) 
+  
 $(document).ready(function () {
     $('.customer-logos').slick({
         slidesToShow: 4,
@@ -29,14 +31,14 @@ $(document).ready(function () {
 
 let items = document.querySelectorAll('.faq');
 
-let length = items.length; 
+let length = items.length;
 
 items.forEach(
-  (item) => {
-    item.addEventListener('click', function(){
-      item.children[1].style.display = item.children[1].style.display==='block' ? item.children[1].style.display = 'none' : item.children[1].style.display = 'block';
-      item.children[0].classList.toggle('to-bottom');
-      item.children[0].classList.toggle('to-top');
-    })
-  }
+    (item) => {
+        item.addEventListener('click', function () {
+            item.children[1].style.display = item.children[1].style.display === 'block' ? item.children[1].style.display = 'none' : item.children[1].style.display = 'block';
+            item.children[0].classList.toggle('to-bottom');
+            item.children[0].classList.toggle('to-top');
+        })
+    }
 )
