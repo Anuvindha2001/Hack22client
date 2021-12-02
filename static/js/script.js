da2fceb48cf9d2fa1babde30f041874898a33cf7
Anuvindha2001 <b19cs015@mace.ac.in>
2021-12-02 13:01:11 +0530

    navbar in progress

--- static/js/script.js
+++ static/js/script.js
@@ -2,37 +2,32 @@ $(window).on('load', function () {
     $('#preloader').hide()
 })
 
-$(document).ready(function () {
-    $('.customer-logos').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 520,
-                settings: {
-                    slidesToShow: 2
-                }
-            }
-        ]
-    })
-})
-const toggles = document.querySelectorAll('.faq-toggle')
-toggles.forEach((toggle) => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
+// $(document).ready(function () {
+//     $('.customer-logos').slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 1000,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: false,
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 3
+//                 }
+//             },
+//             {
+//                 breakpoint: 520,
+//                 settings: {
+//                     slidesToShow: 2
+//                 }
+//             }
+//         ]
+//     })
+// })
+
 let items = document.querySelectorAll('.faq')
 
 let length = items.length
